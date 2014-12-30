c7336be48fcce6dbc9656fca3c45c2f220c204b7
nsarle <nsarle@gmail.com>
2014-12-30 20:52:29 +0100

    gmaps ubication

--- Contacter.php
+++ Contacter.php
@@ -61,7 +61,7 @@
     <section>
                 <div class="container">
                 <div class="row">
-            <iframe src="https://www.google.es/maps/place/Les+Sourciers/@43.917371,0.235619,17z/data=!3m1!4b1!4m2!3m1!1s0x12aa1991ad86cc87:0xcb11aa2990a331a6" width="400" height="400" frameborder="0" style="border:0"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d734066.0374159254!2d0.04623358587646287!3d44.05350942800578!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aa1991ad86cc87%3A0xcb11aa2990a331a6!2sLes+Sourciers!5e0!3m2!1ses!2sfr!4v1419968629506" width="600" height="450" frameborder="0" style="border:0"></iframe>
                 </div>
                 </div>
     </section><br><br>
