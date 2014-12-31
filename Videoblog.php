de824037073c8f968cec7999c279d1172f057681
nsarle <nsarle@gmail.com>
2014-12-31 09:28:42 +0100

    gvideo new list

--- Videoblog.php
+++ Videoblog.php
@@ -6,7 +6,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-xs-12 col-sm-6 col-md-8 col-lg-8 centrarvideo">
-                        <iframe width="853" height="480" src="//www.youtube-nocookie.com/embed/videoseries?list=PLP-NRLO8Nc1nFifbASkH5yfEHgeLFcX58" frameborder="0" allowfullscreen></iframe>
+                        <iframe width="853" height="480" src="//www.youtube.com/embed/yKtU6Vg-zYw?list=PLP-NRLO8Nc1m_l4IhnBerTzVooYo3WTEv" frameborder="0" allowfullscreen></iframe>
                         </div>
                     </div>    
                 </div>
